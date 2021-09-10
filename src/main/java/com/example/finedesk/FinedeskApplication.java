f66cc692b9f3cbe1f6512ad69d30f11a9be6f673
annesukiassian <anna.sukiassian@outlook.com>
2021-09-10 15:40:43 +0400

    initial commit

---
+++ src/main/java/com/example/finedesk/FinedeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.finedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinedeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinedeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finedesk/FinedeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finedesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinedeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
